a834228a1103cce80356268023f6d13f0f243107
DegreeAG <huynghia12081998@gmail.com>
2025-03-07 21:40:08 +0700

    update

--- day13/test_miniproject/src/main/java/com/example/test_miniproject/controller/ProductController.java
+++ day13/test_miniproject/src/main/java/com/example/test_miniproject/controller/ProductController.java
@@ -4,6 +4,7 @@ import com.example.test_miniproject.model.Product;
 import com.example.test_miniproject.service.ProductService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -16,13 +17,21 @@ public class ProductController {
     public ProductController(ProductService productService) {
         this.productService = productService;
     }
-    //Lấy danh sách products
-    @GetMapping("/products")
+
+    @GetMapping //http://localhost:8080
     public ResponseEntity<?> getAllProduct(){
         List<Product> products = productService.getAllBooks();
         return ResponseEntity.ok(products);
     }
 
 
-}
+    @GetMapping("/products/{id}")
+    public ResponseEntity<?> getProductById(@PathVariable int id){
+        Product product = productService.getProductById(id);
+        return ResponseEntity.ok(product);
+    }
 
+
+
+
+}
